08f8b7b34cf094bc292badb4fce5286ab9bdbbbf
Study Hsueh <ph.study@gmail.com>
2019-10-03 12:24:34 +0800

    update init script

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get -y update
+apt -y update && apt -y upgrade
+
 
 # set up a silent install of MySQL
 dbpass=$1
@@ -9,7 +10,7 @@ echo mysql-server-5.7 mysql-server/root_password password $dbpass | debconf-set-
 echo mysql-server-5.7 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
-apt-get -y install apache2 mysql-server php7.2 libapache2-mod-php7.2 php-mysql php-curl php-json php-cgi
+apt-get -y install apache2 mysql-server-5.7 php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-curl php7.2-json php7.2-cgi
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
